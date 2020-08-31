5dfbb6583e306544c5998205900a99b7f2ae990b
remg427@gmail.com <52327a24e16dd744b09cf9f5f9bdd4d2cd1c3794>
2020-08-31 21:13:21 +0200

    1.1.0

--- TA_thehive_ce/bin/ta_thehive_ce_declare.py
+++ TA_thehive_ce/bin/ta_thehive_ce_declare.py
@@ -8,18 +8,11 @@ This file is genrated by Splunk add-on builder
 
 import os
 import sys
-import re
 
 if sys.version_info[0] < 3:
     py_version = "aob_py2"
 else:
     py_version = "aob_py3"
 
-ta_name = 'TA_thehive_ce'
-ta_lib_name = 'lib'
-_SPLUNK_PATH = os.environ['SPLUNK_HOME']
-pattern = re.compile(r"[\\/]etc[\\/]apps[\\/][^\\/]+[\\/]bin[\\/]?$")
-new_paths = [path for path in sys.path if not pattern.search(path) or ta_name in path]
-new_paths.insert(0, os.path.sep.join([_SPLUNK_PATH, 'etc', 'apps', ta_name, ta_lib_name, 'splunklib']))
-new_paths.insert(0, os.path.sep.join([_SPLUNK_PATH, 'etc', 'apps', ta_name, ta_lib_name, py_version]))
-sys.path = new_paths
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib", py_version))
