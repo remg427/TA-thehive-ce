39662213f390a43af8c6685787be89bb9e3bd7dd
remg427@gmail.com <52327a24e16dd744b09cf9f5f9bdd4d2cd1c3794>
2020-08-31 21:11:39 +0200

    1.1.0

--- TA_thehive_ce/bin/ta_thehive_ce_declare.py
+++ TA_thehive_ce/bin/ta_thehive_ce_declare.py
@@ -16,9 +16,10 @@ else:
     py_version = "aob_py3"
 
 ta_name = 'TA_thehive_ce'
-ta_lib_name = 'packages'
+ta_lib_name = 'lib'
+_SPLUNK_PATH = os.environ['SPLUNK_HOME']
 pattern = re.compile(r"[\\/]etc[\\/]apps[\\/][^\\/]+[\\/]bin[\\/]?$")
 new_paths = [path for path in sys.path if not pattern.search(path) or ta_name in path]
-new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name]))
-new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name, py_version]))
+new_paths.insert(0, os.path.sep.join([_SPLUNK_PATH, 'etc', 'apps', ta_name, ta_lib_name, 'splunklib']))
+new_paths.insert(0, os.path.sep.join([_SPLUNK_PATH, 'etc', 'apps', ta_name, ta_lib_name, py_version]))
 sys.path = new_paths
