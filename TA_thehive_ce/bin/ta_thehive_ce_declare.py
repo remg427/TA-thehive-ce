b493c0e014b6f1dd34162705e007edde05c3c6cc
remg427@gmail.com <52327a24e16dd744b09cf9f5f9bdd4d2cd1c3794>
2020-10-16 18:24:55 +0200

    cloud

--- TA_thehive_ce/bin/ta_thehive_ce_declare.py
+++ TA_thehive_ce/bin/ta_thehive_ce_declare.py
@@ -19,6 +19,6 @@ ta_name = 'TA_thehive_ce'
 ta_lib_name = 'lib'
 pattern = re.compile(r"[\\/]etc[\\/]apps[\\/][^\\/]+[\\/]bin[\\/]?$")
 new_paths = [path for path in sys.path if not pattern.search(path) or ta_name in path]
-new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name]))
-new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name, py_version]))
+new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), "..", ta_lib_name]))
+new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), "..", ta_lib_name, py_version]))
 sys.path = new_paths
